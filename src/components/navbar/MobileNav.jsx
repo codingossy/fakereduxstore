043339bf301d6b379db5d62a1e11d42b6d6b2069
ossy vincent <codingossy@gmail.com>
2022-11-15 10:00:09 +0100

    finale

--- src/components/navbar/MobileNav.jsx
+++ src/components/navbar/MobileNav.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './Header.css'
 import {IoClose} from 'react-icons/io5'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 
 const MobileNav = ( {setNavMobile }) => {
+
+  const { carts } = useSelector((state) => state.items);
+
   return (
     <nav className='lg:hidden bg-black text-gray-300 w-full h-full'>
       
@@ -12,8 +17,11 @@ const MobileNav = ( {setNavMobile }) => {
       <ul className='flex flex-col justify-center space-y-8 h-full items-center capitalize font-secondary'>
           <li className='text-2xl'>home</li>
           <li className='text-2xl'>home</li>
-          <li className='text-2xl'>home</li>
-          <li className='text-2xl'>home</li>
+          <Link to={"/cart"}>
+          <li className="rounded-md bg-blue-500 py-2 px-5 capitalize text-white">
+            cart: <span>{carts.length}</span>
+          </li>
+        </Link>
         </ul> 
 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { BiSearch } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 
 // redux
@@ -16,7 +17,7 @@ const Home = () => {
     dispatch(fetchAllProducts());
   }, []);
 
-  const { products, loading } = useSelector((state) => state.items);
+  const { products, loading, carts } = useSelector((state) => state.items);
 
   // handle search iuput
   const [searchInput, setSearchInput] = useState("");
@@ -38,7 +39,7 @@ const Home = () => {
         all products
       </h1>
 
-    <div className="flex items-center justify-center py-4 pb-8">
+    <div className="flex gap-x-5 items-center justify-center py-4 pb-8">
       <form action="" onSubmit={handleSubmit} className='flex items-center gap-x-3'>
         <input
           value={searchInput}
@@ -46,6 +47,14 @@ const Home = () => {
         type="text" className="w-full rounded-md border bg-none p-1 px-2 text-black outline-none placeholder:text-black" placeholder="enter search" />
         <BiSearch size={30} className="cursor-pointer" />
       </form>
+
+      <div>
+      <Link to={"/cart"}>
+          <p className="lg:hidden rounded-md bg-blue-500 py-2 px-5 capitalize text-white">
+            cart: <span>{carts.length}</span>
+          </p>
+        </Link>
+      </div>
     </div>
 
       <div className="grid w-full grid-cols-1 place-items-center gap-x-10 gap-y-10 text-black  sm:grid-cols-1 lg:grid-cols-4">
